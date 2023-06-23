32dd756ee1a7d5b49e5645ffa98e8b7178f73b17
Jayme Borges <86386158+jaymeborges@users.noreply.github.com>
2023-06-23 00:14:23 -0300

    Update main.js

--- main/main.js
+++ main/main.js
@@ -5,7 +5,7 @@ const express = require('express')
 const socketIo = require('socket.io')
 const http = require('http')
 
-// CRIANDO SERVER
+// CRIANDO SERVER //
 
 const app = express ()
 const server = http.greateServer(app)
